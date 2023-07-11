1bb7a99e44eb367fccad4df6d0869279db6b70ff
Guilherme Nascimento de Farias <guiguidefarias@hotmail.com>
2023-07-10 22:00:37 -0300

    NOTNULL

---
+++ NOTNULL.sql
@@ -0,0 +1,13 @@
+USE [Curso]
+
+CREATE TABLE [Aluno] (
+    [Id] INT NOT NULL,
+    [Nome] NVARCHAR(80) NOT NULL,
+    [Nascimento] DATETIME NULL,
+    [Active] BIT NOT NULL DEFAULT(0),
+)
+GO
+
+ALTER TABLE [Aluno]
+    ALTER COLUMN [Active] BIT NOT NULL
+

9bc43da6ba8c0cf9b8bd344ab0d8a6b544c86249
Guilherme Nascimento de Farias <guiguidefarias@hotmail.com>
2023-07-11 19:28:29 -0300

    PrimaryKey
    chave primaria e unique

--- UNIQUE.sql
+++ UNIQUE.sql
@@ -3,10 +3,13 @@ USE [Curso]
 DROP TABLE [Aluno]
 
 CREATE TABLE [Aluno] (
-    [Id] INT NOT NULL UNIQUE,
+    [Id] INT NOT NULL,
     [Nome] NVARCHAR(80) NOT NULL,
-    [Email] NVARCHAR(100) NOT NULL UNIQUE,
+    [Email] NVARCHAR(100) NOT NULL,
     [Nascimento] DATETIME NULL,
     [Active] BIT NOT NULL DEFAULT(0),
+
+    CONSTRAINT [PK_Aluno] PRIMARY KEY([Id]), /* Nomeia e cria a chave primaria*/
+    CONSTRAINT [UQ_Aluno_Email] UNIQUE([Email]), /*Unique -> unico*/
 )
 GO

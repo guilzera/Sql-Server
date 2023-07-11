11281acb1106738b8360d6d5d1f5d5e4379c91df
Guilherme Nascimento de Farias <guiguidefarias@hotmail.com>
2023-07-10 21:40:44 -0300

    first commit

---
+++ CriarTabela.sql
@@ -0,0 +1,13 @@
+USE [Curso]
+
+
+/* ALTER TABLE [Aluno] - Adiciona uma nova coluna
+    ADD [Document] NVARCHAR(11)
+
+ALTER TABLE [Aluno] - Remove uma coluna
+    DROP COLUMN [Document]
+
+
+ALTER TABLE [Aluno] - Muda o tipo (NVARCHAR -> CHAR)
+    ALTER COLUMN [Document] CHAR(11)
+*/

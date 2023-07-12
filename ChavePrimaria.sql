54833da6896a974eccf234e724f1119642dcdd43
Guilherme Nascimento de Farias <guiguidefarias@hotmail.com>
2023-07-12 16:28:01 -0300

    Chave_Composta
    Chave composta e associações.

---
+++ ChavePrimaria.sql
@@ -0,0 +1,48 @@
+USE [Curso]
+
+DROP TABLE [Aluno]
+
+CREATE TABLE [Aluno] (
+    [Id] INT NOT NULL,
+    [Nome] NVARCHAR(80) NOT NULL,
+    [Email] NVARCHAR(100) NOT NULL,
+    [Nascimento] DATETIME NULL,
+    [Active] BIT NOT NULL DEFAULT(0),
+
+    CONSTRAINT [PK_Aluno] PRIMARY KEY([Id]), /* Nomeia e cria a chave primaria*/
+    CONSTRAINT [UQ_Aluno_Email] UNIQUE([Email]), /*Unique -> unico*/
+)
+GO
+
+/*DROP TABLE [Curso] */
+CREATE TABLE [Curso] (
+    [Id] INT NOT NULL,
+    [Nome] NVARCHAR(80) NOT NULL,
+    [CategoriaId] INT NOT NULL,
+
+    CONSTRAINT [PK_Curso] PRIMARY KEY([Id]),
+    CONSTRAINT [FK_Curso_Categoria] FOREIGN KEY([CategoriaId])
+        REFERENCES [Categoria]([Id])
+)
+GO
+
+CREATE TABLE [Categoria] (
+    [Id] INT NOT NULL,
+    [Nome] NVARCHAR(80) NOT NULL,
+
+    CONSTRAINT [PK_Categoria] PRIMARY KEY([Id])
+)
+
+
+ /*Associativa*/
+DROP TABLE [ProgressoCurso]
+
+CREATE TABLE [ProgressoCurso] (
+    [AlunoId] INT NOT NULL,
+    [CursoId] INT NOT NULL,
+    [Progesso] INT NOT NULL,
+    [UltimaAtualizacao] DATETIME NOT NULL DEFAULT(GETDATE()),
+    
+    CONSTRAINT PK_ProgressoCurso PRIMARY KEY ([AlunoId], [CursoId])
+)
+GO

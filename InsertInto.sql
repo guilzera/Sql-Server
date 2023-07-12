6915858aacb55772128c8b97aa74413e2e851d79
Guilherme Nascimento de Farias <guiguidefarias@hotmail.com>
2023-07-12 17:00:33 -0300

    Insert
    Inserindo dados

---
+++ InsertInto.sql
@@ -0,0 +1,13 @@
+INSERT INTO [Categoria]([Nome]) VALUES('Backend')
+INSERT INTO [Categoria]([Nome]) VALUES('Frontend')
+INSERT INTO [Categoria]([Nome]) VALUES('Mobile')
+
+INSERT INTO [Curso]([Nome], [CategoriaId]) VALUES('Fundamentos de C#', 1)
+INSERT INTO [Curso]([Nome], [CategoriaId]) VALUES('Fundamentos de OOP', 1)
+INSERT INTO [Curso]([Nome], [CategoriaId]) VALUES('Angular', 2)
+INSERT INTO [Curso]([Nome], [CategoriaId]) VALUES('Flutter', 3)
+
+INSERT INTO [Curso] VALUES('Flutter e SQLite', 3)
+
+
+

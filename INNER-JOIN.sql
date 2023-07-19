de023ec0c50503d1bcd802262c855f56006a1dcf
Guilherme Nascimento de Farias <guiguidefarias@hotmail.com>
2023-07-19 12:27:23 -0300

    Create INNER-JOIN.sql
    Junção de duas tabelas

---
+++ INNER-JOIN.sql
@@ -0,0 +1,8 @@
+SELECT TOP 100
+    [Curso].[Id],
+    [Curso].[Nome],
+    [Categoria].[Id] AS [Categoria],
+    [Categoria].[Nome]
+FROM
+    [Curso]
+    INNER JOIN [Categoria] ON [Curso].[CategoriaId] = [Categoria].[Id]
